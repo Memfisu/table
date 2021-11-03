09df95b00efd0d17057ba777b34312bffaafc9cc
Memfisu <memfiswho@gmail.com>
2021-11-03 19:07:10 +0400

    feat: React Table
    - DataFlowTest in generators draft

--- src/app/components/DataFlowTest.js
+++ src/app/components/DataFlowTest.js
@@ -1,69 +1,57 @@
 import {Box} from "../../ui-kit/Containers";
 import ButtonWrapper from "../hocs/ButtonWrapper";
 import Button from "../utils/Button";
-import React, {useCallback, useState} from "react";
+import React from "react";
 import axios from "axios";
 import {loadData} from "../reducers/dataLoader";
-import {useDispatch, useSelector} from "react-redux";
 import {setChosenRecord} from "../reducers/recordInfoDemonstrator";
-import {loadedData} from "../selectors/selectors";
-import {clearNewRecord} from "../reducers/newRecordAppendor";
+import { clearNewRecord } from "../reducers/newRecordAppendor";
+import store from "../store";
 
 function* flowWorker (array) {
-    // yield array[0]();
-    // yield array[1]();
-    // yield array[2]();
-    yield console.log('adding a new record');
-    yield console.log('sort ascending');
-    yield console.log('sort descending');
-    // yield console.log('filter');
-    // yield console.log('pagination');
+    for (let i in array) {
+        yield array[i]();
+    }
 }
 
-export const useGenerator = () => {
-    const dispatch = useDispatch();
-
-    const showLoader = useCallback(() => {
-        console.log('loading...');
-    }, []);
-    const hideLoader = useCallback(() => {
-        console.log('loaded');
-    }, []);
-
-    const dataLoad = useCallback(() => {
-        showLoader();
-        axios.get('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
-            .then(res => {
-                dispatch(loadData({data: res.data}));
-            })
-            .catch(err => {
-                console.log(err);
-            })
-            .finally(() => {
-                hideLoader();
-            });
-    }, [dispatch, hideLoader, showLoader]);
-
-    const chosenRecordInfo = useSelector(loadedData);
-    const showAdditionalInfo = useCallback(() => {
-        dispatch(setChosenRecord({ rowData: chosenRecordInfo[0] }));
-    }, [chosenRecordInfo, dispatch]);
-
-    const hideAdditionalInfo = useCallback(() => {
-        dispatch(clearNewRecord());
-    }, [dispatch]);
-
-    return [() => console.log('dataLoad'), () => console.log('showAdditionalInfo'), () => console.log('hideAdditionalInfo')];
+const hideAdditionalInfo = () => {
+    store.dispatch(clearNewRecord);
 };
 
-let counter = 0;
+const showAdditionalInfo = () => {
+    const { dataLoader } = store.getState();
+    store.dispatch(setChosenRecord({ rowData: dataLoader[0] }));
+};
+
+const dataLoad = () => {
+    const showLoader = () => {
+        console.log('loading...');
+    };
+
+    const hideLoader = () => {
+        console.log('loaded');
+    };
+
+    showLoader();
+    axios.get('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+        .then(res => {
+            store.dispatch(loadData({ data: res.data }));
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            hideLoader();
+        });
+};
+
+const steps = [dataLoad, showAdditionalInfo, hideAdditionalInfo];
+
+const temp = flowWorker(steps);
 
 const DataFlowTest = () => {
-    const actions = useGenerator();
-
     const handleClick = () => {
-        actions[counter]();
-        counter++;
+        temp.next();
     };
 
     return (
